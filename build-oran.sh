3715ecda9dec0c506631dd11563de9d21ade98ea
sebdet <sebastien.determe@intl.att.com>
2021-06-30 09:20:31 +0000

    Add helm config for build script
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- build-oran.sh
+++ build-oran.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+helm plugin install ./onap_oom/kubernetes/helm/plugins/undeploy/
+helm plugin install ./onap_oom/kubernetes/helm/plugins/deploy/
+helm plugin install https://github.com/chartmuseum/helm-push.git
+
+helm repo add local http://localhost:8080
+
 echo '### Building ONAP part###'
 (cd onap_oom/kubernetes && make all -e SKIP_LINT=TRUE)
 echo  '### Building ORAN part ###'
