65973c91e633300af085232fbaf014d9257418b4
sebdet <sebastien.determe@intl.att.com>
2021-06-30 07:50:16 +0000

    Fix build script
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- build-oran.sh
+++ build-oran.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo '### Building ONAP part###'
-(cd onap_oom && make all -e SKIP_LINT=TRUE)
+(cd onap_oom/kubernetes && make all -e SKIP_LINT=TRUE)
 echo  '### Building ORAN part ###'
 (cd oran_oom && make all)
