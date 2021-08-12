9bb4e3480c1cbaac6acdd8d65a215052caeccb0f
sebdet <sebastien.determe@intl.att.com>
2021-08-12 14:11:51 +0000

    Add RU simulator draft helm charts
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- scripts/install-oran.sh
+++ scripts/install-oran.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 kubectl create namespace onap
 kubectl create namespace nonrtric
-echo '### Installing ONAP part###'
+kubectl create namespace ricaux
+echo '### Installing ONAP part ###'
 helm deploy --debug onap local/onap --namespace onap -f ../helm-override/onap-override.yaml
-echo  '### Installing ORAN part ###'
-helm install --debug oran local/oran --namespace nonrtric -f ../helm-override/oran-override.yaml
+echo  '### Installing ORAN NONRTRIC part ###'
+helm install --debug oran-nonrtric local/nonrtric --namespace nonrtric -f ../helm-override/oran-override.yaml
+echo '### Installing ORAN RICAUX part ###'
+helm install --debug oran-ricaux local/ricaux --namespace ricaux -f ../helm-override/oran-override.yaml
