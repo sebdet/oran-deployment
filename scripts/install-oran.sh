b3a91f71c0098cc3648a1a23fc1871fc8353b66d
sebdet <sebastien.determe@intl.att.com>
2021-09-10 15:10:19 +0000

    Add cnf helm override and remove ricaux from install script
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- scripts/install-oran.sh
+++ scripts/install-oran.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 kubectl create namespace onap
 kubectl create namespace nonrtric
-kubectl create namespace ricaux
 echo '### Installing ONAP part ###'
 helm deploy --debug onap local/onap --namespace onap -f ../helm-override/onap-override.yaml
 echo  '### Installing ORAN NONRTRIC part ###'
 helm install --debug oran-nonrtric local/nonrtric --namespace nonrtric -f ../helm-override/oran-override.yaml
-echo '### Installing ORAN RICAUX part ###'
-helm install --debug oran-ricaux local/ricaux --namespace ricaux -f ../helm-override/oran-override.yaml
