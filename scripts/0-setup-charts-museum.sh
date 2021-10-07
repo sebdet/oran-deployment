d828a9869fdf318ff3b1ec2012bc4ed476dd7c80
sebdet <sebastien.determe@intl.att.com>
2021-10-07 08:51:22 +0000

    Remove useless DCAE component and remove useless command for chartmuseum
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- scripts/0-setup-charts-museum.sh
+++ scripts/0-setup-charts-museum.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/helm/chartmuseum/main/scripts/get-chartmuseum | bash
-mkdir ./chartmuseum
 chartmuseum --port=18080 --storage="local" --storage-local-rootdir="./chartstorage" &
