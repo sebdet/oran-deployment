a0561ff439deea9384dc793f3ad20b7f9b757183
sebdet <sebastien.determe@intl.att.com>
2021-10-25 17:35:18 +0200

    Change curl to wget
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- scripts/layer-0/0-setup-charts-museum.sh
+++ scripts/layer-0/0-setup-charts-museum.sh
@@ -23,5 +23,5 @@
 # 
 ###
 
-curl https://raw.githubusercontent.com/helm/chartmuseum/main/scripts/get-chartmuseum | bash
+wget https://raw.githubusercontent.com/helm/chartmuseum/main/scripts/get-chartmuseum | bash
 chartmuseum --port=18080 --storage="local" --storage-local-rootdir="./chartstorage" &
