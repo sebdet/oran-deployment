8dfed50243b9dfffa2e154f266aa2c9a930b1a4e
sebdet <sebastien.determe@intl.att.com>
2021-12-16 10:35:14 +0000

    Add devices validation in o1 tests
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- test/pythonsdk/src/orantests/test_o1.py
+++ test/pythonsdk/src/orantests/test_o1.py
@@ -5,16 +5,33 @@ import logging
 from onapsdk.configuration import settings
 from oransdk.dmaap.dmaap import OranDmaap
 import time
+import json
 
 BASIC_AUTH = {}
 
 logging.config.dictConfig(settings.LOG_CONFIG)
 logger = logging.getLogger("test O1")
 
-def test_ru_registration():
+def test_network_devices_registration():
+	# Wait enough time to have at least the SDNR notifications
+	time.sleep(20)
 	logger.info("Verify if SDNR sends well the RU registration to VES by checking in DMAAP")
 	dmaap = OranDmaap()
 	# As the user has been registered in DMAAP during test session init, that call should return all sims registered by SDNR
 	events = dmaap.get_message_from_topic("unauthenticated.VES_PNFREG_OUTPUT", 10000, settings.DMAAP_GROUP, settings.DMAAP_USER).json()
-	#logger.info(f"Json received from DMAAP:{events}")
-	assert (len(events) == 6)
+	# events should be a list of messages
+	logger.info("Verify if the number of events is well equals to the number of devices")
+	assert (len(events) >= 6)
+
+	for event in events:
+		logger.info(f"Checking event: {event}")
+		eventjson = json.loads(event)
+		logger.info("json ?"+str(eventjson))
+		assert(eventjson["event"]["commonEventHeader"]["sourceName"] in settings.NETWORK_SIMULATOR_DEVICES_LIST)
+
+		if "o-ru" in eventjson["event"]["commonEventHeader"]["sourceName"]:
+			logger.info(f"RU event detected checking SDNR has well registered it")
+			assert(eventjson["event"]["commonEventHeader"]["reportingEntityName"] == "ONAP SDN-R")
+		elif "o-du" in eventjson["event"]["commonEventHeader"]["sourceName"]:
+			logger.info(f"DU detected checking it has well registered itself")
+			assert("o-du" in eventjson["event"]["commonEventHeader"]["reportingEntityName"])
