282d15c2d670f0076c1937ddfa18f52f3dde53d8
sebdet <sebastien.determe@intl.att.com>
2022-01-21 17:06:14 +0000

    Introduce new O1 fault tests
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- test/pythonsdk/src/orantests/test_o1.py
+++ test/pythonsdk/src/orantests/test_o1.py
@@ -22,13 +22,14 @@ logger = logging.getLogger("Test O1")
 network_simulators = NetworkSimulators("./resources")
 dmaap = OranDmaap()
 
-@pytest.fixture(scope="class", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def setup_simulators():
     """Setup the simulators before the executing the tests."""
     logger.info("Test class setup for O1 tests")
 
-    # Do a first get to register the o1test/o1test user in DMAAP,
+    # Do a first get to register the o1test/o1test user in DMAAP
     # all registration messages will then be stored for the registration tests.
+    # If it exists already it clears all cached events.
     dmaap.get_message_from_topic("unauthenticated.VES_PNFREG_OUTPUT", 500, settings.DMAAP_GROUP, settings.DMAAP_USER)
 
     network_simulators.start_network_simulators()
@@ -38,10 +39,12 @@ def setup_simulators():
     time.sleep(60)
     logger.info("Enabling faults/events reporting on SDNR")
     network_simulators.enable_events_for_all_simulators()
-
-    ## Preparing the DMaap to cache all the events for the fault topics
+    #time.sleep(3)
+    # Preparing the DMaap to cache all the events for the fault topics.
+    # If it exists already it clears all cached events.
     dmaap.get_message_from_topic("unauthenticated.SEC_FAULT_OUTPUT", 500, settings.DMAAP_GROUP, settings.DMAAP_USER)
-
+    logger.info("Waiting 180s to have registration and faults events in DMaap")
+    time.sleep(180)
     logger.info("Test Session setup completed successfully")
 
     ### Cleanup code
@@ -49,18 +52,31 @@ def setup_simulators():
     network_simulators.stop_network_simulators()
     logger.info("Test Session cleanup done")
 
-def decode_registration_events(events):
+def create_registration_structure(events):
     """Decode the registration events list."""
     devices_found_in_events = dict()
     for event in events:
         event_json = json.loads(event)
-        logger.info("json decoded: %s", str(event_json))
+        logger.info("Registration json decoded: %s", str(event_json))
         devices_found_in_events[event_json["event"]["commonEventHeader"]["sourceName"]] = event_json["event"]["commonEventHeader"]["reportingEntityName"]
 
-    logger.info("devices_found_in_events:%s", devices_found_in_events)
+    logger.info("Devices found in events:%s", devices_found_in_events)
     return devices_found_in_events
 
-def test_network_devices_registration():
+def create_faults_structure(events):
+    """Decode the fault events list."""
+    faults_found_in_events = dict()
+    for event in events:
+        event_json = json.loads(event)
+        logger.info("Fault json decoded: %s", str(event_json))
+        if event_json["event"]["commonEventHeader"]["sourceName"] in faults_found_in_events:
+            faults_found_in_events[event_json["event"]["commonEventHeader"]["sourceName"]] += 1
+        else:
+            faults_found_in_events[event_json["event"]["commonEventHeader"]["sourceName"]] = 1
+    logger.info("Faults found in events: %s", faults_found_in_events)
+    return faults_found_in_events
+
+def test_network_devices_registration_in_dmaap():
     """Validate that the devices are well registered in SDNR and forwarded to VES."""
     logger.info("Verify if SDNR sends well the RU registration to VES by checking in DMAAP")
     # As the user has been registered in DMAAP during test session init,
@@ -70,7 +86,7 @@ def test_network_devices_registration():
     logger.info("Verify if the number of events is well >= to the number of expected devices")
     # The DU can send multiple times message to VES and SDNR can send multiple time event for RU
     assert len(events) >= (len(settings.NETWORK_SIMULATORS_DU_RU_LIST))
-    devices_registered = decode_registration_events(events)
+    devices_registered = create_registration_structure(events)
 
     # Each device must be at least one time in the event list
     for sim_name in settings.NETWORK_SIMULATORS_DU_RU_LIST:
@@ -82,3 +98,16 @@ def test_network_devices_registration():
         elif "o-du" in sim_name:
             logger.info("DU detected checking it has well registered itself")
             assert "o-du" in devices_registered[sim_name]
+
+def test_device_faults_in_dmaap():
+    """Verify that device faults are well sent to DMAAP by SDNR."""
+    logger.info("Verify if SDNR forwards well the faults sent by the simulators to DMAAP")
+    events = dmaap.get_message_from_topic("unauthenticated.SEC_FAULT_OUTPUT", 5000, settings.DMAAP_GROUP, settings.DMAAP_USER).json()
+    logger.info("Verify if faults have well been received for each device")
+    assert len(events) > 0
+    faults_received = create_faults_structure(events)
+
+    # Each device must have some faults
+    for sim_name in settings.NETWORK_SIMULATORS_DU_RU_LIST:
+        logger.info("Check if %s has at least >=3 faults", sim_name)
+        assert sim_name in faults_received and faults_received[sim_name] >= 3
