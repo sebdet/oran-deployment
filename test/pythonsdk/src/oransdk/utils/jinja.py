f2d64a39292d12cbe1b1093a97f8f1a7ecfecb8b
MarineMD <marine.de.dorlodot@intl.att.com>
2022-03-14 19:09:14 +0100

    Add test for the cl_k8s usecase.
    Signed-off-by: MarineMD <marine.de.dorlodot@intl.att.com>

--- test/pythonsdk/src/oransdk/utils/jinja.py
+++ test/pythonsdk/src/oransdk/utils/jinja.py
@@ -48,7 +48,8 @@ def jinja_env() -> Environment:
                            PackageLoader("oransdk.a1sim"),
                            PackageLoader("oransdk.dmaap"),
                            PackageLoader("oransdk.enrichmentservice"),
-                           PackageLoader("oransdk.policy")
+                           PackageLoader("oransdk.policy"),
+                           PackageLoader("oransdk.clamp")
                        ]))
 
 
