313e3b34accca2a28a73f10ccd865b2043b1c52e
MarineMD <marine.de.dorlodot@intl.att.com>
2022-03-15 17:03:34 +0100

    unit-test fixes Signed-off-by: MarineMD <marine.de.dorlodot@intl.att.com>
    Signed-off-by: MarineMD <marine.de.dorlodot@intl.att.com>

--- test/pythonsdk/unit-tests/test_clamp.py
+++ test/pythonsdk/unit-tests/test_clamp.py
@@ -3,7 +3,7 @@
 """Test Clamp module."""
 
 from unittest import mock
-from oransdk.policy.clamp import OranClamp
+from oransdk.policy.clamp import ClampToscaTemplate
 
 
 HEADER={"accept": "application/json", "Content-Type": "application/json"}
@@ -12,39 +12,39 @@ BASE_URL = "http://localhost:8084"
 
 def test_initialization():
     """Class initialization test."""
-    clamp = OranClamp()
-    assert isinstance(clamp, OranClamp)
+    clamp = ClampToscaTemplate()
+    assert isinstance(clamp, ClampToscaTemplate)
 
-@mock.patch.object(OranClamp, 'send_message')
+@mock.patch.object(ClampToscaTemplate, 'send_message')
 def test_upload_commission(mock_send_message):
     """Test Clamp's class method."""
-    OranClamp.upload_commission(BASIC_AUTH)
+    ClampToscaTemplate.upload_commission(BASIC_AUTH)
     url = f"{BASE_URL}/clamp/restservices/clds/v2/toscaControlLoop/commissionToscaTemplate"
     mock_send_message.assert_called_once_with('POST', 'Upload commission', url, basic_auth=BASIC_AUTH)
 
-@mock.patch.object(OranClamp, 'send_message')
+@mock.patch.object(ClampToscaTemplate, 'send_message')
 def test_create_instance(mock_send_message):
     """Test Clamp's class method."""
     dataclamp = {}
-    OranClamp.create_instance(dataclamp, BASIC_AUTH)
+    ClampToscaTemplate.create_instance(dataclamp, BASIC_AUTH)
     url = f"{BASE_URL}/restservices/clds/v2/toscaControlLoop/postToscaInstanceProperties"
     mock_send_message.assert_called_once_with('POST', 'Create Instance', url, data=dataclamp, headers=HEADER, basic_auth=BASIC_AUTH)
 
-@mock.patch.object(OranClamp, 'send_message')
+@mock.patch.object(ClampToscaTemplate, 'send_message')
 def test_change_instance_status(mock_send_message):
     dataclamp={}
-    OranClamp.change_instance_status(dataclamp)
+    ClampToscaTemplate.change_instance_status(dataclamp)
     url = f"{BASE_URL}/restservices/clds/v2/toscaControlLoop/putToscaInstantiationStateChange"
     mock_send_message.assert_called_once_with('PUT', 'Change instance', url, data=dataclamp, headers=HEADER)
 
-@mock.patch.object(OranClamp, 'send_message')
+@mock.patch.object(ClampToscaTemplate, 'send_message')
 def test_delete_instance(mock_send_message):
-    OranClamp.delete_instance()
+    ClampToscaTemplate.delete_instance()
     url = f"{BASE_URL}/restservices/clds/v2/toscaControlLoop/deleteToscaInstanceProperties?name=PMSH_Instance1&version=1.2.3"
     mock_send_message.assert_called_with('DELETE', 'Delete instance', url, headers=HEADER)
 
-@mock.patch.object(OranClamp, 'send_message')
+@mock.patch.object(ClampToscaTemplate, 'send_message')
 def test_decommission_template(mock_send_message):
-    OranClamp.decommission_template()
+    ClampToscaTemplate.decommission_template()
     url = f"{BASE_URL}/restservices/clds/v2/toscaControlLoop/decommissionToscaTemplate?name=ToscaServiceTemplateSimple&version=1.0.0"
     mock_send_message.assert_called_with('DELETE', 'Decommision template', url, headers=HEADER)
