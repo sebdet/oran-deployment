ebc037fa8578b6987d44c87114cf374af3911d73
SebDet <sebastien.determe@intl.att.com>
2022-05-30 10:35:43 +0200

    Update install-onap.sh
    Try to fix strimzi issue

--- scripts/sub-scripts/install-onap.sh
+++ scripts/sub-scripts/install-onap.sh
@@ -24,6 +24,7 @@
 ###
 
 helm repo add strimzi https://strimzi.io/charts/
+helm repo update
 helm install strimzi-kafka-operator strimzi/strimzi-kafka-operator --namespace strimzi-system --version 0.28.0 --set watchAnyNamespace=true --create-namespace
 
 kubectl create namespace onap
