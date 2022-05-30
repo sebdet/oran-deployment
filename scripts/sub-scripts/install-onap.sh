cfb99caae04bc4ea3ed2be4af02505993dc08a77
sebdet <sebastien.determe@intl.att.com>
2022-05-30 11:49:05 +0000

    Attempt to fix the strimzy problem
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- scripts/sub-scripts/install-onap.sh
+++ scripts/sub-scripts/install-onap.sh
@@ -23,9 +23,13 @@
 # 
 ###
 
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+cd $SCRIPT_PATH
 
-helm cm-push strimzi-kafka-operator-helm-3-chart-0.28.0.tgz local
-helm install strimzi-kafka-operator strimzi/strimzi-kafka-operator --namespace strimzi-system --version 0.28.0 --set watchAnyNamespace=true --create-namespace
+helm push ../packages/strimzi-kafka-operator-helm-3-chart-0.28.0.tgz local
+helm repo update
+helm install strimzi-kafka-operator local/strimzi-kafka-operator --namespace strimzi-system --version 0.28.0 --set watchAnyNamespace=true --create-namespace
 
 kubectl create namespace onap
 echo '### Installing ONAP part ###'
